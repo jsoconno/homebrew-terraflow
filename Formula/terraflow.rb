8dd0ddc2d24d9950f09a7e4c6676432ba3850b8b
Justin O'Connor <justinoconnor@justins-air.lan>
2023-03-20 16:43:50 -0400

    change python version to 3.8

--- Formula/terraflow.rb
+++ Formula/terraflow.rb
@@ -7,7 +7,7 @@ class Terraflow < Formula
     sha256 "231dda8aa3408ce27d479114285ba654fb47fbd72d524c1211bc410f063007bb"
     license "GNU Lesser General Public License (LGPL)"
   
-    depends_on "python@3.9"
+    depends_on "python@3.8"
 
     def install
       virtualenv_install_with_resources
