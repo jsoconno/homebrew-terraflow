503095dca001fa875951674e18f36d0e27f04d65
Justin O'Connor <justinoconnor@justins-air.lan>
2023-03-20 08:53:56 -0400

    add homebrew formula

---
+++ Formula/terraflow.rb
@@ -0,0 +1,22 @@
+class Yourproject < Formula
+    include Language::Python::Virtualenv
+  
+    desc "A toolkit for making coding, documenting, and deploying Terraform simpler."
+    homepage "https://github.com/jsoconno/terraflow"
+    url "added-by-github-pipeline-on-release"
+    sha256 "add-by-github-pipeline-on-release"
+    license "GNU Lesser General Public License (LGPL)"
+  
+    depends_on "python@3.10"
+  
+    def install
+      venv = virtualenv_create(libexec, "python3")
+      venv.pip_install_and_link buildpath
+      bin.install_symlink libexec/"bin/terraflow"
+    end
+  
+    test do
+      assert_match "version #{version}", shell_output("#{bin}/terraflow --version")
+    end
+  end
+  
---
+++ README.md
@@ -0,0 +1,11 @@
+# Terraflow
+
+## How do I install these formulae?
+
+`brew install jsoconno/terraflow/<formula>`
+
+Or `brew tap jsoconno/terraflow` and then `brew install <formula>`.
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
