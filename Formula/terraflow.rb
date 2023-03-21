8888399530676a009576269966de3a98c4b2c87b
Justin O'Connor <justinoconnor@justins-air.lan>
2023-03-20 22:23:36 -0400

    test again

--- Formula/terraflow.rb
+++ Formula/terraflow.rb
@@ -22,7 +22,8 @@ class Terraflow < Formula
   def install
     virtualenv_install_with_resources
     libexec.install Dir["*"]
-    (libexec/"terraflow").chmod 0755
+    bin.install libexec/"terraflow"
+    (bin/"terraflow").chmod 0755
     bin.install_symlink libexec/"terraflow"
   end
 
