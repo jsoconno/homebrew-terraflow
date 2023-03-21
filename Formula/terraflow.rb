c74fe10fc43abc5cbf4404220220c3d99289e96f
Justin O'Connor <justinoconnor@Justins-MacBook-Air.local>
2023-03-21 13:52:36 -0400

    revert

--- Formula/terraflow.rb
+++ Formula/terraflow.rb
@@ -3,55 +3,55 @@ class Terraflow < Formula
 
   desc "A toolkit for making coding, documenting, and deploying Terraform simpler."
   homepage "https://github.com/jsoconno/terraflow"
-  url "https://github.com/jsoconno/terraflow/archive/v0.1.3-beta8.tar.gz"
-  sha256 "b60c78242605b5c807152dc37a9c15dba7ad336c67e5a5adfeee0a11fdcbc67d"
+  url "https://github.com/jsoconno/terraflow/archive/v0.1.1.tar.gz"
+  sha256 "9e572c6b26205547b7f2f2cd034e89a2ebf6da7ea16108545da3da422cd6bd60"
   license "GNU Lesser General Public License (LGPL)"
 
   depends_on "python@3.10"
 
   resource "click" do
-    url "https://github.com/jsoconno/terraflow/archive/v0.1.3-beta8.tar.gz"
-    sha256 "b60c78242605b5c807152dc37a9c15dba7ad336c67e5a5adfeee0a11fdcbc67d"
+    url "https://files.pythonhosted.org/packages/59/87/84326af34517fca8c58418d148f2403df25303e02736832403587318e9e8/click-8.1.3.tar.gz"
+    sha256 "7682dc8afb30297001674575ea00d1814d808d6a36af415a82bd481d37ba7b8e"
   end
 
   resource "beautifulsoup4" do
-    url "https://github.com/jsoconno/terraflow/archive/v0.1.3-beta8.tar.gz"
-    sha256 "b60c78242605b5c807152dc37a9c15dba7ad336c67e5a5adfeee0a11fdcbc67d"
+    url "https://files.pythonhosted.org/packages/75/f8/de84282681c5a8307f3fff67b64641627b2652752d49d9222b77400d02b8/beautifulsoup4-4.11.2.tar.gz"
+    sha256 "bc4bdda6717de5a2987436fb8d72f45dc90dd856bdfd512a1314ce90349a0106"
   end
 
   resource "bs4" do
-    url "https://github.com/jsoconno/terraflow/archive/v0.1.3-beta8.tar.gz"
-    sha256 "b60c78242605b5c807152dc37a9c15dba7ad336c67e5a5adfeee0a11fdcbc67d"
+    url "https://files.pythonhosted.org/packages/10/ed/7e8b97591f6f456174139ec089c769f89a94a1a4025fe967691de971f314/bs4-0.0.1.tar.gz"
+    sha256 "36ecea1fd7cc5c0c6e4a1ff075df26d50da647b75376626cc186e2212886dd3a"
   end
 
   resource "requests" do
-    url "https://github.com/jsoconno/terraflow/archive/v0.1.3-beta8.tar.gz"
-    sha256 "b60c78242605b5c807152dc37a9c15dba7ad336c67e5a5adfeee0a11fdcbc67d"
+    url "https://files.pythonhosted.org/packages/9d/ee/391076f5937f0a8cdf5e53b701ffc91753e87b07d66bae4a09aa671897bf/requests-2.28.2.tar.gz"
+    sha256 "98b1b2782e3c6c4904938b84c0eb932721069dfdb9134313beff7c83c2df24bf"
   end
 
   resource "urllib3" do
-    url "https://github.com/jsoconno/terraflow/archive/v0.1.3-beta8.tar.gz"
-    sha256 "b60c78242605b5c807152dc37a9c15dba7ad336c67e5a5adfeee0a11fdcbc67d"
+    url "https://files.pythonhosted.org/packages/21/79/6372d8c0d0641b4072889f3ff84f279b738cd8595b64c8e0496d4e848122/urllib3-1.26.15.tar.gz"
+    sha256 "8a388717b9476f934a21484e8c8e61875ab60644d29b9b39e11e4b9dc1c6b305"
   end
 
   resource "charset-normalizer" do
-    url "https://github.com/jsoconno/terraflow/archive/v0.1.3-beta8.tar.gz"
-    sha256 "b60c78242605b5c807152dc37a9c15dba7ad336c67e5a5adfeee0a11fdcbc67d"
+    url "https://files.pythonhosted.org/packages/ff/d7/8d757f8bd45be079d76309248845a04f09619a7b17d6dfc8c9ff6433cac2/charset-normalizer-3.1.0.tar.gz"
+    sha256 "34e0a2f9c370eb95597aae63bf85eb5e96826d81e3dcf88b8886012906f509b5"
   end
 
   resource "idna" do
-    url "https://github.com/jsoconno/terraflow/archive/v0.1.3-beta8.tar.gz"
-    sha256 "b60c78242605b5c807152dc37a9c15dba7ad336c67e5a5adfeee0a11fdcbc67d"
+    url "https://files.pythonhosted.org/packages/8b/e1/43beb3d38dba6cb420cefa297822eac205a277ab43e5ba5d5c46faf96438/idna-3.4.tar.gz"
+    sha256 "814f528e8dead7d329833b91c5faa87d60bf71824cd12a7530b5526063d02cb4"
   end
 
   resource "certifi" do
-    url "https://github.com/jsoconno/terraflow/archive/v0.1.3-beta8.tar.gz"
-    sha256 "b60c78242605b5c807152dc37a9c15dba7ad336c67e5a5adfeee0a11fdcbc67d"
+    url "https://files.pythonhosted.org/packages/37/f7/2b1b0ec44fdc30a3d31dfebe52226be9ddc40cd6c0f34ffc8923ba423b69/certifi-2022.12.7.tar.gz"
+    sha256 "35824b4c3a97115964b408844d64aa14db1cc518f6562e8d7261699d1350a9e3"
   end
 
   resource "soupsieve" do
-    url "https://github.com/jsoconno/terraflow/archive/v0.1.3-beta8.tar.gz"
-    sha256 "b60c78242605b5c807152dc37a9c15dba7ad336c67e5a5adfeee0a11fdcbc67d"
+    url "https://files.pythonhosted.org/packages/1b/cb/34933ebdd6bf6a77daaa0bd04318d61591452eb90ecca4def947e3cb2165/soupsieve-2.4.tar.gz"
+    sha256 "e28dba9ca6c7c00173e34e4ba57448f0688bb681b7c5e8bf4971daafc093d69a"
   end
 
   def install
@@ -75,4 +75,4 @@ class Terraflow < Formula
   end
 end
 
-sed -i '' 's|^ *url .*\/terraflow\/archive\/.*\"|  url "https://github.com/jsoconno/terraflow/archive/v0.1.3-beta8.tar.gz"
+sed -i '' 's|^ *url .*\/terraflow\/archive\/.*\"|  url "'"$URL"'"\n  sha256 "'"$SHA256"'"/|g' $FORMULA_PATH
