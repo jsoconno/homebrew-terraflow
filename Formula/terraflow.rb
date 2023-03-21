d8d70fdb72b1dbefe2fb143bf3e3d0fddfe60ce3
Justin O'Connor <justinoconnor@justins-air.lan>
2023-03-20 22:38:55 -0400

    add click resource

--- Formula/terraflow.rb
+++ Formula/terraflow.rb
@@ -9,6 +9,11 @@ class Terraflow < Formula
 
   depends_on "python@3.10"
 
+  resource "click" do
+    url "https://files.pythonhosted.org/packages/59/87/84326af34517fca8c58418d148f2403df25303e02736832403587318e9e8/click-8.1.3.tar.gz"
+    sha256 "7682dc8afb30297001674575ea00d1814d808d6a36af415a82bd481d37ba7b8e"
+  end
+
   resource "beautifulsoup4" do
     url "https://files.pythonhosted.org/packages/75/f8/de84282681c5a8307f3fff67b64641627b2652752d49d9222b77400d02b8/beautifulsoup4-4.11.2.tar.gz"
     sha256 "bc4bdda6717de5a2987436fb8d72f45dc90dd856bdfd512a1314ce90349a0106"
@@ -18,6 +23,7 @@ class Terraflow < Formula
     url "https://files.pythonhosted.org/packages/10/ed/7e8b97591f6f456174139ec089c769f89a94a1a4025fe967691de971f314/bs4-0.0.1.tar.gz"
     sha256 "36ecea1fd7cc5c0c6e4a1ff075df26d50da647b75376626cc186e2212886dd3a"
   end
+
 
   def install
     virtualenv_install_with_resources
