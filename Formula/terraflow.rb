2a9c0e1bac8e2d331f23ced31f68e2c8aac1fa5c
Justin O'Connor <justinoconnor@justins-air.lan>
2023-03-20 21:59:20 -0400

    fix formula

--- Formula/terraflow.rb
+++ Formula/terraflow.rb
@@ -1,30 +1,30 @@
 class Terraflow < Formula
-    include Language::Python::Virtualenv
-  
-    desc "A toolkit for making coding, documenting, and deploying Terraform simpler."
-    homepage "https://github.com/jsoconno/terraflow"
-    url "https://github.com/jsoconno/terraflow/archive/v0.1.0.tar.gz"
-    sha256 "231dda8aa3408ce27d479114285ba654fb47fbd72d524c1211bc410f063007bb"
-    license "GNU Lesser General Public License (LGPL)"
-  
-    depends_on "python@3.10"
+  include Language::Python::Virtualenv
 
-    def install
-      virtualenv_install_with_resources
-    end
+  desc "A toolkit for making coding, documenting, and deploying Terraform simpler."
+  homepage "https://github.com/jsoconno/terraflow"
+  url "https://github.com/jsoconno/terraflow/archive/v0.1.0.tar.gz"
+  sha256 "231dda8aa3408ce27d479114285ba654fb47fbd72d524c1211bc410f063007bb"
+  license "GNU Lesser General Public License (LGPL)"
 
-    resource "beautifulsoup4" do
-      url "https://github.com/jsoconno/terraflow/archive/v0.1.0.tar.gz"
-      sha256 "231dda8aa3408ce27d479114285ba654fb47fbd72d524c1211bc410f063007bb"
-    end
-    
-    resource "bs4" do
-      url "https://github.com/jsoconno/terraflow/archive/v0.1.0.tar.gz"
-      sha256 "231dda8aa3408ce27d479114285ba654fb47fbd72d524c1211bc410f063007bb"
-    end
-  
-    test do
-      assert_match "version #{version}", shell_output("#{bin}/terraflow --version")
-    end
+  depends_on "python@3.10"
+
+  resource "beautifulsoup4" do
+    url "https://files.pythonhosted.org/packages/75/f8/de84282681c5a8307f3fff67b64641627b2652752d49d9222b77400d02b8/beautifulsoup4-4.11.2.tar.gz"
+    sha256 "bc4bdda6717de5a2987436fb8d72f45dc90dd856bdfd512a1314ce90349a0106"
   end
-  
+
+  resource "bs4" do
+    url "https://files.pythonhosted.org/packages/10/ed/7e8b97591f6f456174139ec089c769f89a94a1a4025fe967691de971f314/bs4-0.0.1.tar.gz"
+    sha256 "36ecea1fd7cc5c0c6e4a1ff075df26d50da647b75376626cc186e2212886dd3a"
+  end
+
+  def install
+    virtualenv_install_with_resources
+    bin.install "terraflow"
+  end
+
+  test do
+    assert_match "version #{version}", shell_output("#{bin}/terraflow --version")
+  end
+end
